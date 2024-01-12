4efca8d62f362ea2533babd0cb88f93032c7bbcd
Your Name <m.karaarslan06@outlook.com>
2024-01-12 22:33:24 +0300

    stackNavigator and BottomNavigator created

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ManageCourse from './screens/ManageCourse';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import RecentCourses from './screens/RecentCourses';
+import AllCourses from './screens/AllCourses';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const CourseOverview = ()=>{
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="RecentCourses" component={RecentCourses} />
+      <Tab.Screen name="AllCourses" component={AllCourses} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="CourseOverview" options={{headerShown:false}} component={CourseOverview} />
+      <Stack.Screen name="ManageCourse" component={ManageCourse} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
