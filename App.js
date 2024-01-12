c405aa126fd146ca1798de4c7bcf0195c7ec4af3
Your Name <m.karaarslan06@outlook.com>
2024-01-12 22:54:18 +0300

    tabbar styled

--- App.js
+++ App.js
@@ -1,40 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ManageCourse from './screens/ManageCourse';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import RecentCourses from './screens/RecentCourses';
-import AllCourses from './screens/AllCourses';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ManageCourse from "./screens/ManageCourse";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import RecentCourses from "./screens/RecentCourses";
+import AllCourses from "./screens/AllCourses";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const CourseOverview = ()=>{
+const CourseOverview = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="RecentCourses" component={RecentCourses} />
-      <Tab.Screen name="AllCourses" component={AllCourses} />
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "pink" },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: "pink" },
+        tabBarActiveTintColor: "darkblue",
+      }}
+    >
+      <Tab.Screen
+        name="Recent Courses"
+        component={RecentCourses}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="All Courses"
+        component={AllCourses}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="list" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
-}
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="CourseOverview" options={{headerShown:false}} component={CourseOverview} />
-      <Stack.Screen name="ManageCourse" component={ManageCourse} />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="CourseOverview"
+          options={{ headerShown: false }}
+          component={CourseOverview}
+        />
+        <Stack.Screen name="ManageCourse" component={ManageCourse} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
