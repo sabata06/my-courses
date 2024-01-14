c8c1913c29a80b4c15b6a9b228c615f461d58ae7
Your Name <m.karaarslan06@outlook.com>
2024-01-14 19:40:10 +0300

    courses price styled

--- components/Courses.jsx
+++ components/Courses.jsx
@@ -3,20 +3,76 @@ import React from "react";
 import CoursesSummary from "./CoursesSummary";
 import CoursesList from "./CoursesList";
 
+const COURSES = [
+  {
+    id: "1",
+    description: "C Programlama",
+    amount: 69,
+    date: new Date("2023-01-05"),
+  },
+  {
+    id: "2",
+    description: "C# Programlama",
+    amount: 69,
+    date: new Date("2023-04-10"),
+  },
+  {
+    id: "3",
+    description: "Angular",
+    amount: 69,
+    date: new Date("2022-12-08"),
+  },
+  {
+    id: "4",
+    description: "Bootstrap 5",
+    amount: 69,
+    date: new Date("2022-10-10"),
+  },
+  {
+    id: "5",
+    description: "React Js",
+    amount: 69,
+    date: new Date("2023-05-20"),
+  },
+  {
+    id: "6",
+    description: "React Native",
+    amount: 69,
+    date: new Date("2023-07-30"),
+  },
+  {
+    id: "7",
+    description: "Javascript",
+    amount: 69,
+    date: new Date("2023-06-12"),
+  },
+  {
+    id: "8",
+    description: "Komple Web",
+    amount: 69,
+    date: new Date("2021-10-22"),
+  },
+  {
+    id: "9",
+    description: "Frontend",
+    amount: 69,
+    date: new Date("2022-11-25"),
+  },
+];
+
 export default function Courses({ coursesPeriod }) {
   return (
     <View style={styles.container}>
-      <CoursesSummary periodName={coursesPeriod} />
+      <CoursesSummary courses={COURSES} periodName={coursesPeriod} />
       <CoursesList />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        paddingHorizontal:25,
-        paddingTop:25,
-        
-    }
+  container: {
+    flex: 1,
+    paddingHorizontal: 25,
+    paddingTop: 25,
+  },
 });
--- components/CoursesSummary.jsx
+++ components/CoursesSummary.jsx
@@ -1,12 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function CoursesSummary({periodName}) {
+export default function CoursesSummary({periodName, courses}) {
+const coursesSum = courses.reduce((sum,course) => {
+  return sum + course.amount
+}, 0)
+
   return (
-    <View>
-      <Text>{periodName}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{periodName}</Text>
+      <Text style={styles.cost}>{coursesSum + " ₺"}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    backgroundColor:"blue",
+    padding:10,
+    borderRadius:10,
+    alignItems:"center"
+  },
+  title:{
+    color:"white",
+    fontSize:16,
+  },
+  cost:{
+    color:"white",
+    fontSize:16,
+    fontWeight:"bold"
+  }
+})
